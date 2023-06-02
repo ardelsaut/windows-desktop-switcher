44823fdfd1d74c49411cb9d2e54745844f0e058a
ardelsaut <77124020+ardelsaut@users.noreply.github.com>
2023-06-02 13:24:30 +0200

    Create NonoPerso.ps1

---
+++ NonoPerso.ps1
@@ -0,0 +1,12 @@
+﻿
+# Run the following commands in an Administrator powershell prompt. 
+# Be sure to specify the correct path to your desktop_switcher.ahk file. 
+
+$A = New-ScheduledTaskAction -Execute "$env:USERPROFILE\Documents\8.Github\windows-desktop-switcher\desktop_switcher.ahk"
+$T = New-ScheduledTaskTrigger -AtLogon
+$P = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrateurs" -RunLevel Highest
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+Register-ScheduledTask WindowsDesktopSwitcher -InputObject $D -TaskPath "\NonoOs"
+
+timeout.exe /T 3
