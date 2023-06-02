e20907069155f65e5b9f03e1ee3049ce0ea72bc3
ardelsaut <77124020+ardelsaut@users.noreply.github.com>
2023-06-02 13:49:54 +0200

    Update NonoPerso.ps1

--- NonoPerso.ps1
+++ NonoPerso.ps1
@@ -1,12 +1,16 @@
-﻿
-# Run the following commands in an Administrator powershell prompt. 
-# Be sure to specify the correct path to your desktop_switcher.ahk file. 
+﻿# $PathApp = "$env:ProgramFiles\Mozilla Thunderbird\thunderbird.exe"
+# $NameAppTask = "Thunderbird"
+# $GroupIdFrench = "BUILTIN\Administrateurs" # English "BUILTIN\Administrators"
 
-$A = New-ScheduledTaskAction -Execute "$env:USERPROFILE\Documents\8.Github\windows-desktop-switcher\desktop_switcher.ahk"
+$PathApp = "$env:USERPROFILE\Documents\8.Github\windows-desktop-switcher\desktop_switcher.ahk"
+$NameAppTask = "WindowsDesktopSwitcher"
+$GroupIdFrench = "BUILTIN\Administrateurs" # English "BUILTIN\Administrators"
+
+$A = New-ScheduledTaskAction -Execute $PathApp
 $T = New-ScheduledTaskTrigger -AtLogon
-$P = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrateurs" -RunLevel Highest
+$P = New-ScheduledTaskPrincipal -GroupId "$GroupIdFrench" -RunLevel Highest
 $S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
 $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
-Register-ScheduledTask WindowsDesktopSwitcher -InputObject $D -TaskPath "\NonoOs"
+Register-ScheduledTask "$NameAppTask" -InputObject $D -TaskPath "\NonoOs"
 
 timeout.exe /T 3
